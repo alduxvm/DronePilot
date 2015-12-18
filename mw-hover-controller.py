0209ca20b73108fc2de749c098093c3f1ae613b9
Aldo Vargas <alduxvm@gmail.com>
2015-12-18 16:34:45 +0000

    Code clean up after success
    A bit of code clean up after successful PID work.

--- mw-hover-controller.py
+++ mw-hover-controller.py
@@ -8,7 +8,7 @@ __author__ = "Aldo Vargas"
 __copyright__ = "Copyright 2015 Aldux.net"
 
 __license__ = "GPL"
-__version__ = "1"
+__version__ = "1.2"
 __maintainer__ = "Aldo Vargas"
 __email__ = "alduxvm@gmail.com"
 __status__ = "Development"
@@ -18,37 +18,33 @@ from math import *
 from modules.utils import *
 from modules.pyMultiwii import MultiWii
 import modules.UDPserver as udp
-from modules.pid import PID
+
+# Main Configuration
+logging = True
+update_rate = 0.0125 # 80 hz loop cycle
 
 # MRUAV initialization
 vehicle = MultiWii("/dev/ttyUSB0")
-
-# Initialize vehicles attitude for further calculations
 vehicle.getData(MultiWii.ATTITUDE)
-heading = vehicle.attitude['heading']
 
 # Position coordinates [x, y, x] 
 desiredPos = {'x':0.0, 'y':0.0, 'z':0.0} # Set at the beginning (for now...)
 currentPos = {'x':0.0, 'y':0.0, 'z':0.0} # It will be updated using UDP
 
 # Initialize RC commands and pitch/roll to be sent to the MultiWii 
-#rcCMD = [1500,1500,1500,1000,1000,1000,1000,1000]
 rcCMD = [1500,1500,1500,1000]
 desiredRoll = 1500
 desiredPitch = 1500
-maxAngle = 1700
-minAngle = 1300
 
-# PID's initialization 
-rgains = {'kp':0.5, 'ki':0.0, 'kd':0.8, 'iMax':1}
-pgains = {'kp':0.5, 'ki':0.0, 'kd':0.8, 'iMax':1}
+# PID's initialization (Gains are considered the same for pitch and roll)
+gains = {'kp':1.67, 'ki':0.29, 'kd':2.73, 'iMax':1}
 rPIDvalue = 0.0
 pPIDvalue = 0.0
 
-# PID module pids
-rollPID =  PID(rgains['kp'], rgains['ki'], rgains['kd'], 0, 0, rgains['iMax'], -rgains['iMax'])
+# PID modules initialization
+rollPID =  PID(gains['kp'], gains['ki'], gains['kd'], 0, 0, update_rate, gains['iMax'], -gains['iMax'])
 rollPID.setPoint(desiredPos['x'])
-pitchPID = PID(pgains['kp'], pgains['ki'], pgains['kd'], 0, 0, pgains['iMax'], -pgains['iMax'])
+pitchPID = PID(gains['kp'], gains['ki'], gains['kd'], 0, 0, update_rate, gains['iMax'], -gains['iMax'])
 pitchPID.setPoint(desiredPos['y'])
 
 # Function to update commands and attitude to be called by a thread
@@ -68,61 +64,81 @@ def control():
         time.sleep(0.5)
 
     try:
-        #st = datetime.datetime.fromtimestamp(time.time()).strftime('%m_%d_%H-%M-%S')+".csv"
-        #f = open("logs/mw-"+st, "w")
-        #logger = csv.writer(f)
-        #logger.writerow(('timestamp','roll','pitch','yaw','proll','ppitch','throttle','pyaw','x','y','z'))
+        if logging:
+            st = datetime.datetime.fromtimestamp(time.time()).strftime('%m_%d_%H-%M-%S')+".csv"
+            f = open("logs/mw-"+st, "w")
+            logger = csv.writer(f)
+            logger.writerow(('timestamp','Vroll','Vpitch','Vyaw','Proll','Ppitch','Pyaw','Pthrottle','x','y','z','Mroll','Mpitch','Myaw'))
         while True:
-            #elapsed = time.time()
+
+            # Update joystick commands from UDP communication, order (roll, pitch, yaw, throttle)
             rcCMD[0] = udp.message[0]
             rcCMD[1] = udp.message[1]
             rcCMD[2] = udp.message[2]
             rcCMD[3] = udp.message[3]
 
-            # Order of the position from Optitrack is: X, Z, Y
-            currentPos['x'] = udp.message[5]
-            currentPos['y'] = udp.message[4]
+            # Coordinate map from Optitrack in the MAST Lab: X, Y, Z. NED: If going up, Z is negative. 
+            ########## WALL ########
+            #Door       | x+       |
+            #           |          |
+            #           |       y+ |
+            #----------------------|
+            # y-        |          |
+            #           |          |
+            #         x-|          |
+            ########################
+            # Update current position of the vehicle
+            currentPos['x'] = udp.message[4]
+            currentPos['y'] = udp.message[5]
             currentPos['z'] = udp.message[6]
 
-            # Update vehicle attitude
-            #vehicle.getData(MultiWii.ATTITUDE)
+            # Update Attitude 
+            vehicle.getData(MultiWii.ATTITUDE)
 
-            # Update PID's
-            rPIDvalue = rollPID.update(currentPos['x'])
-            pPIDvalue = pitchPID.update(currentPos['y'])
+            # Heading update and/or selection
+            heading = udp.message[9]
 
-            # Calculate angles
-            desiredRoll  = mapping(rPIDvalue, -rgains['iMax'], rgains['iMax'], minAngle, maxAngle )
-            desiredPitch = mapping(pPIDvalue, -pgains['iMax'], pgains['iMax'], minAngle, maxAngle )
+            # PID updating, Roll is for Y and Pitch for X
+            rPIDvalue = rollPID.update(currentPos['y'])
+            pPIDvalue = pitchPID.update(currentPos['x'])
+            
+            # Heading must be in radians, MultiWii heading comes in degrees, optitrack in radians
+            sinYaw = sin(heading)
+            cosYaw = cos(heading)
+
+            # Conversion from desired accelerations to desired angle commands
+            desiredRoll  = toPWM(degrees( (rPIDvalue * cosYaw - pPIDvalue * sinYaw) * (1 / g) ),1)
+            desiredPitch = toPWM(degrees( (pPIDvalue * cosYaw + rPIDvalue * sinYaw) * (1 / g) ),1)
 
             # Limit commands for safety
             if udp.message[7] == 1:
-                rcCMD[1] = limit(desiredRoll, minAngle , maxAngle)
-                rcCMD[0] = limit(desiredPitch, minAngle, maxAngle)
+                rcCMD[0] = limit(desiredRoll,1200,1800)
+                rcCMD[1] = limit(desiredPitch,1200,1800)
+            else:
+                # Prevent integrators/derivators to increase if they are not in use
+                rollPID.resetIntegrator()
+                pitchPID.resetIntegrator()
             rcCMD = [limit(n,1000,2000) for n in rcCMD]
 
-            # Send command to vehicle
-            vehicle.sendCMD(16,MultiWii.SET_RAW_RC,rcCMD)
+            # Send commands to vehicle
+            vehicle.sendCMD(8,MultiWii.SET_RAW_RC,rcCMD)
 
-            # Print information for debugging purposes 
-            print "%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %d %d %d %d" % (time.time(), \
-                    vehicle.attitude['angx'], vehicle.attitude['angy'], udp.message[10], \
-                    currentPos['x'], currentPos['y'], currentPos['z'], \
-                    rPIDvalue, pPIDvalue, \
-                    int(rcCMD[0]), int(rcCMD[1]), \
-                    int(desiredRoll), int(desiredPitch) ) 
+            row =   (current, \
+                        vehicle.attitude['angx'], vehicle.attitude['angy'], vehicle.attitude['heading'], \
+                        #vehicle.rawIMU['ax'], vehicle.rawIMU['ay'], vehicle.rawIMU['az'], vehicle.rawIMU['gx'], vehicle.rawIMU['gy'], vehicle.rawIMU['gz'], \
+                        #vehicle.rcChannels['roll'], vehicle.rcChannels['pitch'], vehicle.rcChannels['throttle'], vehicle.rcChannels['yaw'], \
+                        udp.message[0], udp.message[1], udp.message[2], udp.message[3], \
+                        udp.message[4], udp.message[5], udp.message[6], \
+                        udp.message[8], udp.message[9], udp.message[10])
+            if logging:
+                logger.writerow(row)
 
-            # Save log
-            #logger.writerow((time.time(), \
-            #                 vehicle.attitude['angx'], vehicle.attitude['angy'], vehicle.attitude['heading'], \
-                             #vehicle.rcChannels['roll'], vehicle.rcChannels['pitch'], vehicle.rcChannels['throttle'], vehicle.rcChannels['yaw'], \
-            #                 udp.message[0], udp.message[1], udp.message[3], udp.message[2], \
-            #                 udp.message[5], udp.message[4], udp.message[6] ))
-            time.sleep(0.01) # 100hz 
+            print row
+            # Wait time (not ideal, but its working) 
+            time.sleep(update_rate)  
 
     except Exception,error:
         print "Error in control thread: "+str(error)
-        #f.close()
 
 if __name__ == "__main__":
     try:
@@ -136,4 +152,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print "Keyboard Interrupt, exiting."
         exit()
-
