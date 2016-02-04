0fd063e52096e1d73794762f39b73a0cc0b66ae9
Aldo Vargas <alduxvm@gmail.com>
2016-02-04 10:04:47 +0000

    Update desiredPos inside 100hz loop
    Waypoint not working, this is a test to see if it works better when the
    position is being activated inside the high rate controller.

--- mw-waypoint.py
+++ mw-waypoint.py
@@ -8,7 +8,7 @@ __author__ = "Aldo Vargas"
 __copyright__ = "Copyright 2015 Altax.net"
 
 __license__ = "GPL"
-__version__ = "1.0"
+__version__ = "1.2"
 __maintainer__ = "Aldo Vargas"
 __email__ = "alduxvm@gmail.com"
 __status__ = "Development"
@@ -26,6 +26,7 @@ vehicle_weight = 0.84 # Kg
 u0 = 1000 # Zero throttle command
 uh = 1360 # Hover throttle command
 kt = vehicle_weight * g / (uh-u0)
+ky = 500 / pi # Yaw controller gain
 
 # MRUAV initialization
 vehicle = MultiWii("/dev/ttyUSB0")
@@ -40,22 +41,24 @@ rcCMD = [1500,1500,1500,1000]
 desiredRoll = 1500
 desiredPitch = 1500
 desiredThrottle = 1000
+desiredYaw = 1500
 mode = 'Manual'
+changeWP = False
 
 # Controller PID's gains (Gains are considered the same for pitch and roll)
 p_gains = {'kp': 2.61, 'ki':0.57, 'kd':3.41, 'iMax':2, 'filter_bandwidth':50} # Position Controller gains
 h_gains = {'kp': 4.64, 'ki':1.37, 'kd':4.55, 'iMax':2, 'filter_bandwidth':50} # Height Controller gains
+y_gains = {'kp': 1.0,  'ki':0.0,  'kd':0.0,  'iMax':2, 'filter_bandwidth':50} # Yaw Controller gains
 
 # PID modules initialization
 rollPID =   PID(p_gains['kp'], p_gains['ki'], p_gains['kd'], p_gains['filter_bandwidth'], 0, 0, update_rate, p_gains['iMax'], -p_gains['iMax'])
 rPIDvalue = 0.0
-rollPID.setPoint(desiredPos['x'])
 pitchPID =  PID(p_gains['kp'], p_gains['ki'], p_gains['kd'], p_gains['filter_bandwidth'], 0, 0, update_rate, p_gains['iMax'], -p_gains['iMax'])
 pPIDvalue = 0.0
-pitchPID.setPoint(desiredPos['y'])
 heightPID = PID(h_gains['kp'], h_gains['ki'], h_gains['kd'], h_gains['filter_bandwidth'], 0, 0, update_rate, h_gains['iMax'], -h_gains['iMax'])
 hPIDvalue = 0.0
-heightPID.setPoint(desiredPos['z'])
+yawPID =    PID(y_gains['kp'], y_gains['ki'], y_gains['kd'], y_gains['filter_bandwidth'], 0, 0, update_rate, y_gains['iMax'], -y_gains['iMax'])
+yPIDvalue = 0.0
 
 # Filters initialization
 f_yaw   = low_pass(20,update_rate)
@@ -68,16 +71,14 @@ f_roll  = low_pass(20,update_rate)
 def go_to(target):
     global desiredPos, currentPos
     global rollPID, pitchPID, heightPID
+    global changeWP
 
     timeout = 20
     min_distance = 0.07
     start = time.time()
-    desiredPos = target
 
-    # Update PID values
-    rollPID.setPoint(desiredPos['y'])
-    pitchPID.setPoint(desiredPos['x'])
-    heightPID.setPoint(desiredPos['z'])
+    # Update flag to change WP
+    changeWP = True 
 
     while 'Auto' in mode:
         current = time.time() - start
@@ -85,9 +86,11 @@ def go_to(target):
         print " > %0.2f Going->[%0.1f, %0.1f, %0.1f], actual->[%0.1f, %0.1f, %0.1f], distance to target = %0.4f" % (current,desiredPos['x'],desiredPos['y'],desiredPos['z'],currentPos['x'],currentPos['y'],currentPos['z'],distance)
         if distance <= min_distance:
             print " - Reached target location"
+            changeWP = False
             break;
         if current  >= timeout:
             print " - Timeout to reach location"
+            changeWP = False
             break;
         time.sleep(0.1)
 
@@ -98,22 +101,14 @@ def mission():
         # Only start the mission if the vehicle mode is set to Auto, this check 
         if 'Auto' in mode:
 
-            # Waypoint definition
-            wp1 = {'x':0.0, 'y':0.0, 'z':1.5}
-            wp2 = {'x':0.0, 'y':0.0, 'z':1.0}
-
             print "Starting automatic mission!!"
             time.sleep(1)
 
-            print "Traveling to waypoint 1: ", wp1
+            print "Travelling to way-point 1: ", wp1
             go_to(wp1)
             # Maintain current position for certain time
             time.sleep(5)
 
-            print "Traveling to waypoint 2: ", wp1
-            go_to(wp2)
-            # Maintain current position for certain time
-            time.sleep(5)
         else:
             print "Mode: %s | Z: %0.3f | X: %0.3f | Y: %0.3f " % (mode, currentPos['z'], currentPos['x'], currentPos['y'])
             time.sleep(0.5)
@@ -145,6 +140,9 @@ def flight_management():
             logger.writerow(('timestamp','Vroll','Vpitch','Vyaw','Proll','Ppitch','Pyaw','Pthrottle', \
                              'x','y','z','Dx','Dy','Dz','Mroll','Mpitch','Myaw','Mode','Croll','Cpitch','Cyaw','Cthrottle'))
         while True:
+            # Variable to time the loop
+            current = time.time()
+            elapsed = 0
 
             # Update joystick commands from UDP communication, order (roll, pitch, yaw, throttle)
             rcCMD[0] = udp.message[0]
@@ -173,10 +171,16 @@ def flight_management():
             # Filter new values before using them
             heading = f_yaw.update(udp.message[9])
 
+            if changeWP:
+                desiredPos = {'x':0.0, 'y':0.0, 'z':1.5}
+            else:
+                desiredPos = {'x':0.0, 'y':0.0, 'z':1.0}
+
             # PID updating, Roll is for Y and Pitch for X, Z is negative
-            rPIDvalue = rollPID.update(currentPos['y'])
-            pPIDvalue = pitchPID.update(currentPos['x'])
-            hPIDvalue = heightPID.update(currentPos['z'])
+            rPIDvalue = rollPID.update(desiredPos['y'] - currentPos['y'])
+            pPIDvalue = pitchPID.update(desiredPos['x'] - currentPos['x'])
+            hPIDvalue = heightPID.update(desiredPos['z'] - currentPos['z'])
+            yPIDvalue = yawPID.update(0.0 - heading)
             
             # Heading must be in radians, MultiWii heading comes in degrees, optitrack in radians
             sinYaw = sin(heading)
@@ -187,11 +191,13 @@ def flight_management():
             desiredPitch = toPWM(degrees( (pPIDvalue * cosYaw - rPIDvalue * sinYaw) * (1 / g) ),1)
             desiredThrottle = ((hPIDvalue + g) * vehicle_weight) / (cos(f_pitch.update(radians(vehicle.attitude['angx'])))*cos(f_roll.update(radians(vehicle.attitude['angy']))))
             desiredThrottle = (desiredThrottle / kt) + u0
+            desiredYaw = 1500 - (yPIDvalue * ky)
 
             # Limit commands for safety
             if udp.message[7] == 1:
                 rcCMD[0] = limit(desiredRoll,1000,2000)
                 rcCMD[1] = limit(desiredPitch,1000,2000)
+                rcCMD[2] = limit(desiredYaw,1000,2000)
                 rcCMD[3] = limit(desiredThrottle,1000,2000)
                 mode = 'Auto'
             else:
@@ -199,6 +205,7 @@ def flight_management():
                 rollPID.resetIntegrator()
                 pitchPID.resetIntegrator()
                 heightPID.resetIntegrator()
+                yawPID.resetIntegrator()
                 mode = 'Manual'
             rcCMD = [limit(n,1000,2000) for n in rcCMD]
 
@@ -220,8 +227,9 @@ def flight_management():
             #print "\tMode: %s | Z: %0.3f | X: %0.3f | Y: %0.3f " % (mode, currentPos['z'], currentPos['x'], currentPos['y'])
             if 'Manual' in mode:
                 print "\tMode: %s | Z: %0.3f | X: %0.3f | Y: %0.3f " % (mode, desiredPos['z'], desiredPos['x'], desiredPos['y'])
-            # Wait time (not ideal, but its working) 
-            time.sleep(update_rate)  
+            # Wait until the update_rate is completed 
+            while elapsed < update_rate:
+                elapsed = time.time() - current 
 
     except Exception,error:
         print "Error in control thread: "+str(error)
