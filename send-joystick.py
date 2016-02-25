1b1dff89696151fc11a45b04239b8cfb2db3c5f1
Aldo Vargas <alduxvm@gmail.com>
2016-02-25 18:19:22 +0000

    Working Joystick example

--- send-joystick.py
+++ send-joystick.py
@@ -17,22 +17,40 @@ import pygame
 from modules.utils import *
 
 # Main configuration
-# IP address of the vehicle (raspberry pi address) 
-UDP_IP = "127.0.0.1"
+#UDP_IP = "127.0.0.1" # Localhost (for testing)
+UDP_IP = "130.209.176.146" # Vehicle IP address
 UDP_PORT = 51001 # This port match the ones using on other scripts
 
 update_rate = 0.01 # 100 hz loop cycle
 # Create UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+try:
+    pygame.init()
+    pygame.joystick.init()
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+except Exception,error:
+    print "No joystick connected on the computer, "+str(error)
+
 while True:
     current = time.time()
     elapsed = 0
+    
+    # Joystick reading
+    pygame.event.pump()
+    roll     = mapping(joystick.get_axis(0),-1.0,1.0,1000,2000)
+    pitch    = mapping(joystick.get_axis(1),1.0,-1.0,1000,2000)
+    yaw      = mapping(joystick.get_axis(2),-1.0,1.0,1000,2000)
+    throttle = mapping(joystick.get_axis(3),1.0,-1.0,1000,2000)
+    mode     = joystick.get_button(24)
 
     # Be sure to always send the data as floats
-    message = [1500.0, 1500.0, 1500.0, 1000.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    # The extra zeros on the message are there in order for the other scripts to do not complain about missing information
+    message = [roll, pitch, yaw, throttle, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     buf = struct.pack('>' + 'd' * len(message), *message)
     sock.sendto(buf, (UDP_IP, UDP_PORT))
+    
     print message
 
     # Make this loop work at update_rate
